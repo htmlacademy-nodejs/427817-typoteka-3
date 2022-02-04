da6806354fac6ddb814b9b86c7e8fd008ffd0e07
naryzhnaj <naryzhnaj@gmail.com>
2022-02-05 02:01:08 +0300

    загрузка из текстовых

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -12,54 +12,11 @@ const DEFAULT_COUNT = 1;
 const RECORDS_LIMIT = 1000;
 const SENTENCES_LIMIT = 5;
 const FILE_NAME = `mocks.json`;
-
-const TITLES = [
-  `Ёлки. История деревьев`,
-  `Как перестать беспокоиться и начать жить`,
-  `Как достигнуть успеха не вставая с кресла`,
-  `Обзор новейшего смартфона`,
-  `Лучшие рок-музыканты 20-века`,
-  `Как начать программировать`,
-  `Учим HTML и CSS`,
-  `Что такое золотое сечение`,
-  `Как собрать камни бесконечности`,
-  `Борьба с прокрастинацией`,
-  `Рок — это протест`,
-  `Самый лучший музыкальный альбом этого года`
-];
-const SENTENCES = [
-  `Ёлки — это не просто красивое дерево. Это прочная древесина.`,
-  `Первая большая ёлка была установлена только в 1938 году.`,
-  `Вы можете достичь всего. Стоит только немного постараться и запастись книгами.`,
-  `Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
-  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
-  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
-  `Освоить вёрстку несложно. Возьмите книгу новую книгу и закрепите все упражнения на практике.`,
-  `Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.`,
-  `Программировать не настолько сложно, как об этом говорят.`,
-  `Простые ежедневные упражнения помогут достичь успеха.`,
-  `Это один из лучших рок-музыкантов.`,
-  `Он написал больше 30 хитов.`,
-  `Из под его пера вышло 8 платиновых альбомов.`,
-  `Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.`,
-  `Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?`,
-  `Достичь успеха помогут ежедневные повторения.`,
-  `Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
-  `Как начать действовать? Для начала просто соберитесь.`,
-  `Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравятся только игры.`,
-  `Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.`
-];
-const CATEGORIES = [
-  `Деревья`,
-  `За жизнь`,
-  `Без рамки`,
-  `Разное`,
-  `IT`,
-  `Музыка`,
-  `Кино`,
-  `Программирование`,
-  `Железо`
-];
+const FILE_PATHS = {
+  SENTENCES: `./data/sentences.txt`,
+  TITLES: `./data/titles.txt`,
+  CATEGORIES: `./data/categories.txt`
+};
 
 /**
  * Возвращает случайную дату в рамках одного квартала
@@ -74,18 +31,38 @@ const getRecentRandomDate = () => {
 /**
  * Формирует массив со случайными тестовыми данными
  *
- * @param {Integer} count
+ * @param {Integer} count запрашиваемое число готовых записей
+ * @param {Array} sentences
+ * @param {Array} titles
+ * @param {Array} categories
  * @return {Array}
  */
-const generateCards = (count) => {
-  const randomText = shuffle(SENTENCES);
+const generateCards = (count, sentences, titles, categories) => {
+  const randomText = shuffle(sentences);
+
   return Array(count).fill({}).map(() => ({
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
+    title: titles[getRandomInt(0, titles.length - 1)],
     announce: randomText.slice(0, getRandomInt(1, SENTENCES_LIMIT)).join(` `),
     fullText: randomText.slice(0, getRandomInt(SENTENCES_LIMIT, randomText.length - 1)).join(` `),
     createdDate: getRecentRandomDate().toLocaleString(),
-    category: shuffle(CATEGORIES).slice(0, getRandomInt(1, SENTENCES_LIMIT))
+    category: shuffle(categories).slice(0, getRandomInt(1, SENTENCES_LIMIT))
   }));
+};
+
+/**
+ * Считывает данные из текстового файла
+ *
+ * @param {String} filePath имя файла
+ * @return {Array} массив считанных строк
+ */
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+    return content.split(`\n`).map((s) => s.trim());
+  } catch (err) {
+    console.error(chalk.red(`Can't read data from ${filePath}...`));
+    return [];
+  }
 };
 
 module.exports = {
@@ -97,7 +74,13 @@ module.exports = {
     if (countCard > RECORDS_LIMIT) {
       return console.error(`Извините, не допускается больше ${RECORDS_LIMIT} публикаций`);
     }
-    const content = JSON.stringify(generateCards(countCard));
+
+    const [sentences, titles, categories] = await Promise.all([
+      readContent(FILE_PATHS.SENTENCES),
+      readContent(FILE_PATHS.TITLES),
+      readContent(FILE_PATHS.CATEGORIES)
+    ]);
+    const content = JSON.stringify(generateCards(countCard, sentences, titles, categories));
 
     try {
       await fs.writeFile(FILE_NAME, content);
