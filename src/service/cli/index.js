521d1ea5424c1e7f63d4e3f2e1aadf4474883884
naryzhnaj <naryzhnaj@gmail.com>
2022-02-14 02:34:18 +0300

    запуск сервера

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -2,11 +2,13 @@
 
 const help = require(`./help`);
 const generate = require(`./generate`);
+const server = require(`./server`);
 const version = require(`./version`);
 
 const Cli = {
   [generate.name]: generate,
   [help.name]: help,
+  [server.name]: server,
   [version.name]: version,
 };
 module.exports = {
